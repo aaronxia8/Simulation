8a047eb0862cd34e5ad5c8529a0a93d82ad8c8b2
Aaronxia <yizhouxia8@gmail.com>
2018-10-26 13:51:27 -0500

    Upload simulation to github

---
+++ Simu_10_26_2018.R
@@ -0,0 +1,194 @@
+##Part 1 for homogeneous Markov chain with 3 states {1,2,3}
+
+## define a function to get n samples from a given discrete distribution p 
+sampleDist = function(n,p) { 
+  sample(x = c(1,2,3), n, replace = T, prob = p) 
+}
+## set the predefine transition matrix
+p1=c(0.5,0.25,0.25)
+p2=c(1/3,1/3,1/3)
+p3=c(0.25,0.5,0.25)
+
+## define a function to record the length of longest consecutive visits of state 1 
+## with time length lent
+LCV2 <- function(lent,p1,p2,p3){
+  currentpos=1
+  ccount=0
+  hcount=0
+  for (i in (1:lent)){
+    if (currentpos==1){
+      currentpos=sampleDist(1,p1)
+      if (currentpos==1){
+        ccount=ccount+1
+        if (ccount>=hcount){
+          hcount=ccount
+        }
+      }
+      else{
+        ccount=0
+      }
+    }
+    else if (currentpos==2){
+      currentpos=sampleDist(1,p2)
+      if (currentpos==1){
+        ccount=ccount+1
+        if (ccount>=hcount){
+          hcount=ccount
+        }
+      }
+      else{
+        ccount=0
+      }
+    }
+    else{
+      currentpos=sampleDist(1,p3)
+      if (currentpos==1){
+        ccount=ccount+1
+        if (ccount>=hcount){
+          hcount=ccount
+        }
+      }
+      else{
+        ccount=0
+      }
+      
+    }
+  }
+  return (hcount)
+}
+
+##derive the mean of L(1,10000) for 5000 runs
+tr=c()
+for (i in (1:5000)){
+  tr=c(tr,LCV2(10000,p1,p2,p3))
+}
+mean(tr)
+
+
+##derive the mean of L(1,100000) for 500 runs
+tr=c()
+for (i in (1:500)){
+  tr=c(tr,LCV2(100000,p1,p2,p3))
+}
+mean(tr)
+
+## a function to derive the sum of p_11(l) from l=1 to lent
+findsump <- function(lent){
+sump=0
+init=matrix(c(1,0,0),ncol=3,nrow=1)
+tm1=t(matrix(c(0.5,0.25,0.25,1/3,1/3,1/3,0.25,0.5,0.25), ncol = 3, nrow = 3))
+tm=t(matrix(c(0.5,0.25,0.25,1/3,1/3,1/3,0.25,0.5,0.25), ncol = 3, nrow = 3))
+sump=c(init%*%tm)[1]
+for (i in (2:lent)){
+  tm=tm%*%tm1
+  sump=sump+c(init%*%tm)[1]
+}
+return (sump)
+}
+
+##find the quantity of sum of p_11(l) from l=1 to lent for lent=10000 and lent=100000
+findsump(10000)
+findsump(100000)
+
+
+##Pat 2 Simulation for time nonhomogeneous Markov chain with 3 states {1,2,3}
+## generate all random number needed for P(t) from t=1 to 100000
+set.seed(100)
+RG=sample(1:30,8*100000,replace=TRUE)
+## define a function to generate the discrete distribution for each time t from sequence RG
+getdist<-function(RG,i,s){
+  if (s==1){
+    x=RG[(8*(i-1)+1):(8*(i-1)+2)]
+    a=sum(x)
+    x=c(a,x)
+    p=x/sum(x)
+  }
+  else if (s==2){
+    x=RG[(8*(i-1)+3):(8*(i-1)+5)]
+    p=x/sum(x)
+  }
+  else {
+    x=RG[(8*(i-1)+6):(8*(i-1)+8)]
+    p=x/sum(x)
+  }
+  return (p)
+}
+
+LCV3 <- function(lent,RG){
+  currentpos=1
+  ccount=0
+  hcount=0
+  for (i in (1:lent)){
+    if (currentpos==1){
+      p1=getdist(RG,i,1)
+      currentpos=sampleDist(1,p1)
+      if (currentpos==1){
+        ccount=ccount+1
+        if (ccount>=hcount){
+          hcount=ccount
+        }
+      }
+      else{
+        ccount=0
+      }
+    }
+    else if (currentpos==2){
+      p2=getdist(RG,i,2)
+      currentpos=sampleDist(1,p2)
+      if (currentpos==1){
+        ccount=ccount+1
+        if (ccount>=hcount){
+          hcount=ccount
+        }
+      }
+      else{
+        ccount=0
+      }
+    }
+    else{
+      p3=getdist(RG,i,3)
+      currentpos=sampleDist(1,p3)
+      if (currentpos==1){
+        ccount=ccount+1
+        if (ccount>=hcount){
+          hcount=ccount
+        }
+      }
+      else{
+        ccount=0
+      }
+      
+    }
+  }
+  return (hcount)
+}
+
+## a function to derive the sum of p_11(l) from l=1 to lent
+findsump <- function(lent,RG){
+  sump=0
+  init=matrix(c(1,0,0),ncol=3,nrow=1)
+  tm=t(matrix(c(getdist(RG,1,1),getdist(RG,1,2),getdist(RG,1,3)), ncol = 3, nrow = 3))
+  sump=c(init%*%tm)[1]
+  for (i in (2:lent)){
+    tm=tm%*%t(matrix(c(getdist(RG,i,1),getdist(RG,i,2),getdist(RG,i,3)), ncol = 3, nrow = 3))
+    sump=sump+c(init%*%tm)[1]
+  }
+  return (sump)
+}
+findsump(10000,RG)
+findsump(100000,RG)
+
+##derive the mean of L(1,10000) for 5000 runs
+tr=c()
+for (i in (1:5000)){
+  tr=c(tr,LCV3(10000,RG))
+}
+mean(tr)
+
+
+##derive the mean of L(1,100000) for 500 runs
+tr=c()
+for (i in (1:500)){
+  tr=c(tr,LCV3(100000,RG))
+}
+mean(tr)
