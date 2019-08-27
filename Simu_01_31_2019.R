fc9adf64715e2a2342dc3606dbdb2ac654b91835
aaronxia8 <36425716+aaronxia8@users.noreply.github.com>
2019-08-27 09:18:57 -0500

    Update Simu_01_31_2019.R

--- Simu_01_31_2019.R
+++ Simu_01_31_2019.R
@@ -456,6 +456,108 @@ findsump(10000)
 findsump(100000)
 
 
+
+##plot
+##A.1 for homogeneous Markov chain with 3 states {1,2,3}
+set.seed(100)
+## define a function to get n samples from a given discrete distribution p 
+sampleDist = function(n,p) { 
+  sample(x = c(1,2,3), n, replace = T, prob = p) 
+}
+## set the predefine transition matrix
+p1=c(0.5,0.25,0.25)
+p2=c(1/3,1/3,1/3)
+p3=c(0.25,0.5,0.25)
+
+## a function to derive the sum of p_11(l) from l=1 to lent
+
+sump=c()
+sump1=0
+init=matrix(c(1,0,0),ncol=3,nrow=1)
+tm1=t(matrix(c(0.5,0.25,0.25,1/3,1/3,1/3,0.25,0.5,0.25), ncol = 3, nrow = 3))
+tm=t(matrix(c(0.5,0.25,0.25,1/3,1/3,1/3,0.25,0.5,0.25), ncol = 3, nrow = 3))
+sump1=c(init%*%tm)[1]
+sump=c(sump,log2(sump1))
+for (i in (2:1000)){
+  tm=tm%*%tm1
+  sump1=sump1+c(init%*%tm)[1]
+  sump=c(sump,log2(sump1))
+}
+
+
+
+## define a function to record the length of longest consecutive visits of state 1 
+## with time length lent
+LCV2 <- function(lent,p1,p2,p3){
+  currentpos=1
+  ccount=0
+  hcount=0
+  s1=c()
+  for (i in (1:lent)){
+    if (currentpos==1){
+      currentpos=sampleDist(1,p1)
+      if (currentpos==1){
+        ccount=ccount+1
+        if (ccount>hcount){
+          hcount=ccount
+        }
+        s1=c(s1,hcount)
+      }
+      else{
+        ccount=0
+        s1=c(s1,hcount)
+      }
+    }
+    else if (currentpos==2){
+      currentpos=sampleDist(1,p2)
+      if (currentpos==1){
+        ccount=ccount+1
+        if (ccount>hcount){
+          hcount=ccount
+        }
+        s1=c(s1,hcount)
+      }
+      else{
+        ccount=0
+        s1=c(s1,hcount)
+      }
+    }
+    else{
+      currentpos=sampleDist(1,p3)
+      if (currentpos==1){
+        ccount=ccount+1
+        if (ccount>hcount){
+          hcount=ccount
+        }
+        s1=c(s1,hcount)
+      }
+      else{
+        ccount=0
+        s1=c(s1,hcount)
+      }
+      
+    }
+  }
+  return (s1)
+}
+
+##derive the mean of L(1,10000) for 5000 runs
+tr=c()
+for (i in (1:10000)){
+  tr=c(tr,LCV2(1000,p1,p2,p3))
+}
+s1=t(matrix(tr,ncol=10000))
+meany=colMeans(s1,dims=1)
+x1=seq(3,1000)
+
+##plot figure 1 in thesis (comparsion of 3 estimates of E(L(n)) and simulations)
+df1 <- data.frame("Length" = c(x1,x1), "Value" = c(sump[3:1000],meany[3:1000]),"Type"=c(rep("Our estimation",998),rep("Simulation",998)))
+d <- ggplot(df1, aes(Length, Value))
+d  + geom_point(aes(colour = Type),size=1,alpha=0.8,position=position_jitter(h=0.1, w=0.1)) +  xlab("Number of trials n") + ylab("Expected value of L(1,n) in simulations ")+ theme_grey(base_size = 24)+ theme(legend.position = c(0.8, 0.2))
+
+
+
+
 ##A.2 Simulation for time nonhomogeneous Markov chain with 3 states {1,2,3}
 ## generate all random number needed for P(t) from t=1 to 100000
 set.seed(100)
@@ -560,6 +662,118 @@ for (i in (1:1500)){
 mean(tr)
 quantile(tr)
 quantile(tr, probs = c(0.05, 0.95))
+
+##plot
+##A.2 Simulation for time nonhomogeneous Markov chain with 3 states {1,2,3}
+## generate all random number needed for P(t) from t=1 to 1000
+sampleDist = function(n,p) { 
+  sample(x = c(1,2,3), n, replace = T, prob = p) 
+}
+
+set.seed(100)
+RG=sample(1:30,8*1000,replace=TRUE)
+## define a function to generate the discrete distribution for each time t from sequence RG
+getdist<-function(RG,i,s){
+  if (s==1){
+    x=RG[(8*(i-1)+1):(8*(i-1)+2)]
+    a=sum(x)
+    x=c(a,x)
+    p=x/sum(x)
+  }
+  else if (s==2){
+    x=RG[(8*(i-1)+3):(8*(i-1)+5)]
+    p=x/sum(x)
+  }
+  else {
+    x=RG[(8*(i-1)+6):(8*(i-1)+8)]
+    p=x/sum(x)
+  }
+  return (p)
+}
+## a function to derive the sum of p_11(l) from l=1 to lent
+  sump=C()
+  sump1=0
+  init=matrix(c(1,0,0),ncol=3,nrow=1)
+  tm=t(matrix(c(getdist(RG,1,1),getdist(RG,1,2),getdist(RG,1,3)), ncol = 3, nrow = 3))
+  sump=c(init%*%tm)[1]
+  for (i in (2:1000)){
+    tm=tm%*%t(matrix(c(getdist(RG,i,1),getdist(RG,i,2),getdist(RG,i,3)), ncol = 3, nrow = 3))
+    sump1=sump1+c(init%*%tm)[1]
+    sump=c(sump,log2(sump1))
+  }
+
+
+LCV3 <- function(lent,RG){
+  currentpos=1
+  ccount=0
+  hcount=0
+  s1=c()
+  for (i in (1:lent)){
+    if (currentpos==1){
+      p1=getdist(RG,i,1)
+      currentpos=sampleDist(1,p1)
+      if (currentpos==1){
+        ccount=ccount+1
+        if (ccount>=hcount){
+          hcount=ccount
+        }
+        s1=c(s1,hcount)
+      }
+      else{
+        ccount=0
+        s1=c(s1,hcount)
+      }
+    }
+    else if (currentpos==2){
+      p2=getdist(RG,i,2)
+      currentpos=sampleDist(1,p2)
+      if (currentpos==1){
+        ccount=ccount+1
+        if (ccount>=hcount){
+          hcount=ccount
+        }
+        s1=c(s1,hcount)
+      }
+      else{
+        ccount=0
+        s1=c(s1,hcount)
+      }
+    }
+    else{
+      p3=getdist(RG,i,3)
+      currentpos=sampleDist(1,p3)
+      if (currentpos==1){
+        ccount=ccount+1
+        if (ccount>=hcount){
+          hcount=ccount
+        }
+        s1=c(s1,hcount)
+      }
+      else{
+        ccount=0
+        s1=c(s1,hcount)
+      }
+      
+    }
+  }
+  return (s1)
+}
+
+
+##derive the mean of L(1,10000) for 5000 runs
+tr=c()
+for (i in (1:10000)){
+  tr=c(tr,LCV3(1000,RG))
+}
+s1=t(matrix(tr,ncol=10000))
+meany=colMeans(s1,dims=1)
+x1=seq(3,1000)
+
+##plot figure 1 in thesis (comparsion of 3 estimates of E(L(n)) and simulations)
+df1 <- data.frame("Length" = c(x1,x1), "Value" = c(sump[3:1000],meany[3:1000]),"Type"=c(rep("Our estimation",998),rep("Simulation",998)))
+d <- ggplot(df1, aes(Length, Value))
+d  + geom_point(aes(colour = Type),size=1,alpha=0.8,position=position_jitter(h=0.1, w=0.1)) +  xlab("Number of trials n") + ylab("Expected value of L(1,n) in simulations ")+ theme_grey(base_size = 24)+ theme(legend.position = c(0.8, 0.2))
+
 
 ##A.2.2 a nonhomogeneous Markov chain with p_11(l)=1/(l+1)
 
@@ -679,6 +893,124 @@ GA[1]
 mean(tr)
 quantile(tr)
 quantile(tr, probs = c(0.05, 0.95))
+
+
+##plot
+## define a function to generate the desired transition matrix at time t to ganrantee that p_11(t)=(1/(t+1))^0.1
+simulateTransition<-function(pi,t){
+  if (t==1){
+    p1=runif(1,0,1/2)
+    p2=1/2-p1
+    p3=runif(1,0,1)
+    p4=runif(1,0,1-p3)
+    p5=1-p3-p4
+    p6=runif(1,0,1)
+    p7=runif(1,0,1-p6)
+    p8=1-p6-p7
+    pt=c(1/2,p1,p2,p3,p4,p5,p6,p7,p8)
+  }
+  else{
+    x0=pi[1]
+    x1=pi[2]
+    x2=pi[3]
+    p1=runif(1,0,1/2)
+    p2=1/2-p1
+    p3=min(((1/(t+1))-(0.5)*x0)/(x1+x2),0.99)
+    p4=runif(1,0,1-p3)
+    p5=1-p3-p4
+    p6=min(((1/(t+1))-(0.5)*x0)/(x1+x2),0.99)
+    p7=runif(1,0,1-p6)
+    p8=1-p6-p7
+    pt=c(1/2,p1,p2,p3,p4,p5,p6,p7,p8)
+  }
+  return (pt)
+}
+
+##define a function to generate all transition matrix from t=1 to lent
+
+  pi=c(1,0,0)
+  alltransi=c()
+  sum1=0
+  sump=c()
+  for (i in (1:1000)){
+    pp=simulateTransition(pi,i)
+    pi=pi%*%t(matrix(pp, ncol = 3, nrow = 3))
+    sum1=sum1+c(pi)[1]
+    sump=c(sump,log2(sum1))
+    alltransi=c(alltransi,pp)
+  }
+
+
+LCV4<- function(lent,GA){
+  currentpos=1
+  ccount=0
+  hcount=0
+  ss1=c()
+  for (i in (1:lent)){
+    if (currentpos==1){
+      p1=GA[((i-1)*9+1):((i-1)*9+3)]
+      currentpos=sampleDist(1,p1)
+      if (currentpos==1){
+        ccount=ccount+1
+        if (ccount>=hcount){
+          hcount=ccount
+        }
+        ss1=c(ss1,hcount)
+      }
+      else{
+        ccount=0
+        ss1=c(ss1,hcount)
+      }
+    }
+    else if (currentpos==2){
+      p2=GA[((i-1)*9+4):((i-1)*9+6)]
+      currentpos=sampleDist(1,p2)
+      if (currentpos==1){
+        ccount=ccount+1
+        if (ccount>=hcount){
+          hcount=ccount
+        }
+        ss1=c(ss1,hcount)
+      }
+      else{
+        ccount=0
+        ss1=c(ss1,hcount)
+      }
+    }
+    else{
+      p3=GA[((i-1)*9+7):((i-1)*9+9)]
+      currentpos=sampleDist(1,p3)
+      if (currentpos==1){
+        ccount=ccount+1
+        if (ccount>=hcount){
+          hcount=ccount
+        }
+        ss1=c(ss1,hcount)
+      }
+      else{
+        ccount=0
+        ss1=c(ss1,hcount)
+      }
+      
+    }
+  }
+  return (ss1)
+}
+set.seed(100)
+##derive the mean of L(1,10000) for 2500 runs
+tr=c()
+for (i in (1:10000)){
+  tr=c(tr,LCV4(1000,alltransi))
+}
+s1=t(matrix(tr,ncol=10000))
+meany=colMeans(s1,dims=1)
+x1=seq(3,1000)
+
+##plot figure 1 in thesis (comparsion of 3 estimates of E(L(n)) and simulations)
+df1 <- data.frame("Length" = c(x1,x1), "Value" = c(sump[3:1000],meany[3:1000]),"Type"=c(rep("Our estimation",998),rep("Simulation",998)))
+d <- ggplot(df1, aes(Length, Value))
+d  + geom_point(aes(colour = Type),size=1,alpha=0.8,position=position_jitter(h=0.1, w=0.1)) +  xlab("Number of trials n") + ylab("Expected value of L(1,n) in simulations ")+ theme_grey(base_size = 24)+ theme(legend.position = c(0.8, 0.2))
+
 
 
 ##a counterexample where the state is transient
